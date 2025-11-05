90795cb595dca79a795d6b6c798723eeb153e60f
elenaGonzalez <elebeatrizgonzakez@gmail.com>
2025-11-05 19:25:40 -0300

    publicaciones CRUD

---
+++ back/src/Handler/publicacionesHandler.js
@@ -0,0 +1,64 @@
+const {
+  getPublicacionController,
+  getPublicacionesController,
+  postPublicacionController,
+  putPublicacionController,
+  deletePublicacionController,
+} = require("../Controller/publicacionesController");
+
+const getPublicacionesHandler = async (req, res) => {
+  try {
+    let publicaciones = await getPublicacionesController();
+    res.status(200).send(publicaciones);
+  } catch (error) {
+    res.status(500).send({ Error: error.message });
+  }
+};
+
+const getPublicacionHandler = async (req, res) => {
+  const { id } = req.params;
+  try {
+    let publicacion_buscada = await getPublicacionController(id);
+    res.status(200).send(publicacion_buscada);
+  } catch (error) {
+    res.status(500).send({ Error: error.message });
+  }
+};
+
+const postPublicacionHandler = async (req, res) => {
+  try {
+    const {titulo, descripcion, telefono, nombre, genero, edad, vacunado, raza, foto, ciudad} = req.body;
+    const publicacion = await postPublicacionController(titulo, descripcion, telefono, nombre, genero, edad, vacunado, raza, foto, ciudad);
+    res.status(200).send(publicacion);
+} catch (error) {
+    res.status(500).send({ Error: error.message });
+  }
+};
+
+const putPublicacionHandler = async (req, res) => {
+  const {id, titulo,} = req.body;
+    try {
+    let publicaciones = await putPublicacionController(id);
+    res.status(200).send(publicaciones);
+  } catch (error) {
+    res.status(500).send({ Error: error.message });
+  }
+};
+
+const deletePublicacionHandler = async (req, res) => {
+    const { id } = req.params;
+  try {
+    let publicacion_eliminar = await deletePublicacionController(id);
+    res.status(200).send(publicacion_eliminar);
+  } catch (error) {
+    res.status(500).send({ Error: error.message });
+  }
+};
+
+module.exports = {
+  getPublicacionHandler,
+  getPublicacionesHandler,
+  postPublicacionHandler,
+  putPublicacionHandler,
+  deletePublicacionHandler,
+};
