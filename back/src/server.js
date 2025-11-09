3507ca9c56111487e77bd73f7f0e9a832b291fe1
Benitez Andres <benitezandres104@gmail.com>
2025-11-09 17:32:36 -0300

    FEAT: Implementación final del carrusel

--- back/src/server.js
+++ back/src/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const cors = require('cors'); // ⬅️ 1. Importar el módulo CORS
 
 const app = express();
+
+// ⬅️ 2. Aplicar el middleware CORS
+app.use(cors()); 
+
 app.use(express.json());
 
 const mainRoute = require('./Routes/mainRoute.js');
